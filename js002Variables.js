bc5200ee54f631fbe9a0ebc2b9ccf6cd4aaac68e
RudrakshaSingh <rudrakshasingh587@gmail.com>
2024-06-21 08:47:26 -0700

    2

---
+++ js002Variables.js
@@ -0,0 +1,19 @@
+//memoryspace for saving values
+//let
+//const  -  cannot be changed
+//var    -  not used due to issue block scope and functional scope , hosting
+
+//camelCasing
+const accountId = 2144;
+let accountEmail = "hit@gmail.com";
+var accountPassword = "1233";
+accountCity = "jaipur"; //bad method
+let accountState; // will give undefined
+
+/* accountId = 2;  //not allowed
+ console.log(accountId);*/
+
+accountEmail = "vcrfv@gmail.com";
+accountPassword = "523";
+accountCity = "gb";
+console.log(accountId, accountPassword, accountCity);
