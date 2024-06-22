bcb5531a1ebee16227077fdec7bd9bf2cce36b3a
RudrakshaSingh <rudrakshasingh587@gmail.com>
2024-06-22 06:06:02 -0700

    operations

---
+++ js005Operations.js
@@ -0,0 +1,37 @@
+/* operations  */
+
+let value = 3;
+let negativeValuee = -value;
+console.log(negativeValuee);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//complex situations
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+//bad
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
