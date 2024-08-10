93fb4759b44eb75da9bac1cf7794d0e60c8bd35e
Rudraksha Singh <rudrakshasingh587@gmail.com>
2024-08-10 08:15:04 +0530

    empty

--- 01-Basics/js001.js
+++
@@ -1,9 +0,0 @@
-//shortcut - log
-//how to run - in terminal
-//node filename.js
-//js is prototype based language
-console.log("rudra");
-console.log("hi");
-
-//js has prototypal behaaviour//goes to parent then grandparent
-//stops at object prototype
